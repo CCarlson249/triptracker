725c7f0e5a1ba02704623a6e17d922af864d3118
Clayton Carlson <claytonrc97@gmail.com>
2023-04-27 12:48:46 -0700

    models and database built

--- server/config.py
+++ server/config.py
@@ -7,12 +7,14 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from flask_bcrypt import Bcrypt
 
 # Local imports
-
+#$ python -c 'import os; print(os.urandom(16))'   generate secret key in CLI
 # Instantiate app, set attributes
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.secret_key = b'\x90e\x1c\x8a\x03\x0f\x0b\xd1\xd8\x1b"\x90\xcb\xe1\x93"'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///delivery.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
@@ -21,6 +23,7 @@ metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 db = SQLAlchemy(metadata=metadata)
+bcrypt = Bcrypt(app)
 migrate = Migrate(app, db)
 db.init_app(app)
 
--- server/models.py
+++ server/models.py
@@ -1,5 +1,88 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.hybrid import hybrid_property
+from datetime import datetime
 
-from config import db
+from config import db, bcrypt
 
-# Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+    serialize_rules = ('-created_at', '-updated_at', '-trips')
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    trips = db.relationship('Trip', backref='user')
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Password hashes may not be viewed.')
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+class Trip(db.Model, SerializerMixin):
+    __tablename__ = 'trips'
+
+    serialize_rules = ('-created_at', '-updated_at', '-events', )
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    depart_day = db.Column(db.Date, nullable=False)
+    depart_time = db.Column(db.Time, nullable=False)
+    arrive_day = db.Column(db.Date, nullable=False)
+    arrive_time = time = db.Column(db.Time, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    flight_number = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    events = db.relationship('Event', backref='trip')
+
+
+class Event(db.Model, SerializerMixin):
+    __tablename__ = 'events'
+
+    serialize_rules = ('-created_at', '-updated_at')
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+    date = db.Column(db.Date, nullable=False)
+    time = db.Column(db.Time)
+    description = db.Column(db.String)
+    trip_id = db.Column(db.Integer, db.ForeignKey('trips.id'))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+class Location(db.Model, SerializerMixin):
+    __tablename__ = 'locations'
+
+    serialize_rules = ('-created_at', '-updated_at')
+                       
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+trip_locations = db.Table('trip_locations',
+    db.Column('trip_id', db.Integer, db.ForeignKey('trips.id')),
+    db.Column('location_id', db.Integer, db.ForeignKey('locations.id'))
+)

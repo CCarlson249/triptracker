0268c50dbd28d5adc14ff33f68cf948e4b0ba9c9
Clayton Carlson <claytonrc97@gmail.com>
2023-05-10 01:59:41 -0700

    event mapping added

---
+++ client/src/components/ActivityCard.js
@@ -0,0 +1,10 @@
+import {useState, useEffect} from 'react';
+
+
+function ActivityCard(){
+    return (
+        <div>An activity</div>
+    )
+}
+
+export default ActivityCard;
--- client/src/components/TripPage.js
+++ client/src/components/TripPage.js
@@ -1,30 +1,42 @@
 import {useState, useEffect} from 'react'
 import {useParams} from 'react-router-dom';
-
-
-
-
+import ActivityCard from './ActivityCard';
 
 function TripPage(){
     const [trips, setTrips] = useState([])    
+    const [tripEvents, setTripEvents] = useState([])
     let params = useParams();
     
-     useEffect(() =>{
+    useEffect(() =>{
         fetch(`/trips/${params.id}`)
         .then(res => res.json())
         .then(data => {
-        setTrips(data)
-    
-console.log(data)})
-        
+            setTrips(data)
+            console.log(data)
+        })
     }, [params.id])
 
-    console.log(trips)
-return (
-    <div>
-        <h1>{trips.name}</h1>
+    useEffect(() =>{
+        fetch('/events')
+        .then(res => res.json())
+        .then(events => {
+            console.log(events)
+            const filteredEvents = events.filter(event => event.trip_id === trips.id)
+            console.log(filteredEvents)
+            setTripEvents(filteredEvents)
+        })
+    }, [trips.id])
+
+    return (
+        <div>
+            <div>
+                <h1>{trips.name}</h1>
+            </div>
+            <div>
+                {tripEvents.map(tripEvent => <ActivityCard tripEvent={tripEvent}/>)}
+            </div>
         </div>
-)
+    )
 }
 
 export default TripPage;

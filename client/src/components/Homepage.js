68b391edd7f4c9eb0e16f6f5e4e103804c6e384d
Clayton Carlson <claytonrc97@gmail.com>
2023-05-15 21:12:26 -0700

    state issues fixed

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,18 +1,24 @@
-
 import React, { useEffect } from 'react';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import TripCard from './TripCard';
 import { tripsAtom } from './atoms';
-import {useState} from 'react';
+import axios from 'axios';
 
-
-
-function Homepage({user}) {
-
-
+function Homepage({ user }) {
+  const setTrips = useSetRecoilState(tripsAtom);
   const trips = useRecoilValue(tripsAtom);
  
-console.log(trips);
+  useEffect(() => {
+    // Fetch new data from /trips endpoint and update the tripsAtom state
+    axios.get('/trips')
+      .then(response => {
+        setTrips(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching trips:', error);
+      });
+  }, [setTrips]);
+
   const userTrips = trips.filter(trip => trip.user_id === user.id);
 
   return (
@@ -23,6 +29,6 @@ console.log(trips);
       ))}
     </div>
   );
-      }
+}
 
 export default Homepage;
